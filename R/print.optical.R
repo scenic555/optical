8fad2452e375541e6ebb9d7a54f09439ed808f83
scenic5555 <scenic555@gmail.com>
2023-05-27 21:50:01 +0200

    print file

--- R/print.optical.R
+++ R/print.optical.R
@@ -3,6 +3,7 @@
 #' This function print the table of interval boundaries for
 #' optimal design with items and probabilities.
 #' @param x optical object.
+#' @param digits significant digits in printout.
 #' @param ... other print arguments.
 #' @return No return value, called for side effects.
 #' @usage \method{print}{optical}(x,digits=max(3, getOption("digits")-3),\dots)
